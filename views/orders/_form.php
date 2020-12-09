fad9d28f868710b455f3ab49c1ca9ceadf862675
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2020-12-09 09:11:15 +0200

    Update _form.php
    dd

--- views/orders/_form.php
+++ views/orders/_form.php
@@ -15,7 +15,7 @@ $order_id = rand(1, 999999999999);
 
 
 if (!$model->isNewRecord) {
- $order_id=$model->order_id;
+    $order_id=$model->order_id;
 }
 /* @var $this yii\web\View */
 /* @var $model app\models\orders\Orders */
@@ -24,17 +24,14 @@ if (!$model->isNewRecord) {
 
 <div class="container">
 
-<?php $form = ActiveForm::begin(['id' => 'dynamic-form', 'options' => ['enctype' => 'multipart/form-data']]); ?>
+    <?php $form = ActiveForm::begin(['id' => 'dynamic-form', 'options' => ['enctype' => 'multipart/form-data']]); ?>
     <div class="row">
         <div class="col-md-4">
-             <?= $form->field($model, 'order_id')->textInput(['maxlength' => true,'value'=>$order_id,'disabled'=>true]) ?>
-             <?= $form->field($model, 'name')->textInput() ?>
-             <?= $form->field($model, 'phone')->textInput() ?>
-             <?= $form->field($model, 'other_phone')->textInput() ?>
-             <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
-        </div>
-        <div class="col-md-4">
-         
+            <?= $form->field($model, 'order_id')->textInput(['maxlength' => true,'value'=>$order_id,'disabled'=>true]) ?>
+            <?= $form->field($model, 'name')->textInput() ?>
+            <?= $form->field($model, 'phone')->textInput() ?>
+            <?= $form->field($model, 'other_phone')->textInput() ?>
+            <?= $form->field($model, 'address')->textInput(['maxlength' => true]) ?>
             <?= $form->field($model, 'delivery_date')->widget(DatePicker::classname(), [
                 'options' => ['placeholder' => Yii::t('app', 'Enter_date')],
                 'value' => Carbon::now('Asia/Amman')->toDateString(),
@@ -52,56 +49,45 @@ if (!$model->isNewRecord) {
 
                 ]
             ]); ?>
-
-
-            
             <?= $form->field($model, 'delivery_time')->widget(TimePicker::classname(), []);?>
-        
-            
-             <?= $form->field($model, 'country_id')->widget(Select2::classname(), [
-                        'data' =>  ArrayHelper::map(Countries::find()->all(), 'id', 'name_ar'),
-                        'language' => 'ar',
-                        'options' => ['placeholder' =>Yii::t('app',"Plz_Select")],
-                       
-                    ]); ?>
-            <?= $form->field($model, 'region_id')->widget(Select2::classname(), [
-                        'data' =>  ArrayHelper::map(Regions::find()->all(), 'id', 'name_ar'),
-                        'language' => 'ar',
-                        'options' => ['placeholder' =>Yii::t('app',"Plz_Select")],
-                       
-                    ]); ?>
-
-            <?= $form->field($model, 'area_id')->widget(Select2::classname(), [
-                        'data' =>  ArrayHelper::map(Area::find()->all(), 'id', 'name_ar'),
-                        'language' => 'ar',
-                        'options' => ['placeholder' =>Yii::t('app',"Plz_Select")],
-                       
-                    ]); ?>
-            
             <?= $form->field($model, 'status_id')->widget(Select2::classname(), [
-                        'data' =>  ArrayHelper::map(Status::find()->all(), 'id', 'name_ar'),
-                        'language' => 'ar',
-                        'options' => ['placeholder' =>Yii::t('app',"Plz_Select")],
-                       
-                    ]); ?>
+                'data' =>  ArrayHelper::map(Status::find()->all(), 'id', 'name_ar'),
+                'language' => 'ar',
+                'options' => ['placeholder' =>Yii::t('app',"Plz_Select"),'id'=>'status_id'],
 
+            ]); ?>
+            <?= $form->field($model, 'country_id')->widget(Select2::classname(), [
+                'data' =>  ArrayHelper::map(Countries::find()->all(), 'id', 'name_ar'),
+                'language' => 'ar',
+                'options' => ['placeholder' =>Yii::t('app',"Plz_Select"),'id'=>'country_id'],
+
+            ]); ?>
+            <?= $form->field($model, 'region_id')->widget(Select2::classname(), [
+                'data' =>  ArrayHelper::map(Regions::find()->all(), 'id', 'name_ar'),
+                'language' => 'ar',
+                'options' => ['placeholder' =>Yii::t('app',"Plz_Select"),'id'=>'region_id'],
+
+            ]); ?>
+            <?= $form->field($model, 'area_id')->widget(Select2::classname(), [
+                'data' =>  ArrayHelper::map(Area::find()->all(), 'id', 'name_ar'),
+                'language' => 'ar',
+                'options' => ['placeholder' =>Yii::t('app',"Plz_Select"),'id'=>'area_id'],
+
+            ]); ?>
         </div>
         <div class="col-md-4">
             <div class="row">
-                    <?php include('order_items.php') ?>
-                </div>
+                <?php include('order_items.php') ?>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <?= $form->field($model, 'discount')->textInput(['id'=>'discount']) ?>
+            <?= $form->field($model, 'delivery_price')->textInput(['id'=>'delivery_price']) ?>
+            <?= $form->field($model, 'total_price')->textInput(['id'=>'total_price']) ?>
+            <?= $form->field($model, 'amount_required')->textInput(['id'=>'amount_required']) ?>
         </div>
     </div>
-
-
-    
-
-
-
-
-   
-
-   
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
