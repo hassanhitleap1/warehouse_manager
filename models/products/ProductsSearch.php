96e9a1e349ef980edf01cae01b6c3c34950d8c19
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2020-12-14 10:22:45 +0200

    Update ProductsSearch.php
    video_url

--- models/products/ProductsSearch.php
+++ models/products/ProductsSearch.php
@@ -18,7 +18,7 @@ class ProductsSearch extends Products
     {
         return [
             [['id', 'quantity', 'category_id', 'status', 'supplier_id', 'unit_id', 'warehouse_id'], 'integer'],
-            [['name', 'thumbnail', 'created_at', 'updated_at'], 'safe'],
+            [['name', 'thumbnail','video_url', 'created_at', 'updated_at'], 'safe'],
             [['purchasing_price', 'selling_price'], 'number'],
         ];
     }
@@ -73,6 +73,7 @@ class ProductsSearch extends Products
         ]);
 
         $query->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'video_url', $this->video_url])
             ->andFilterWhere(['like', 'thumbnail', $this->thumbnail]);
 
         return $dataProvider;
