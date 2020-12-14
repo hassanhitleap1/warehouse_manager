1313df46f49299dd7def38967d4842e706984217
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2020-12-14 11:04:14 +0200

    Update StatusController.php
    BaseController

--- controllers/StatusController.php
+++ controllers/StatusController.php
@@ -12,7 +12,7 @@ use yii\filters\VerbFilter;
 /**
  * StatusController implements the CRUD actions for Status model.
  */
-class StatusController extends Controller
+class StatusController extends BaseController 
 {
     /**
      * {@inheritdoc}
