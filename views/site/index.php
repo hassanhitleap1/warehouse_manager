766f079d02d4b88f07bcc61a937419514bfc6f4b
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2020-12-14 12:17:33 +0200

    Update index.php
    index

--- views/site/index.php
+++ views/site/index.php
@@ -3,6 +3,32 @@
 /* @var $this yii\web\View */
 
 $this->title = 'روند';
+use yii\widgets\LinkPager;
+use yii\helpers\Html;
 ?>
-
+<div class="container">
     
+    <div class="row">
+		<?php foreach ($models as  $key => $model) : ?>
+            <div class="col-md-4>
+                <div class="panel-group">
+                  <div class="panel panel-primary">
+                    <div class="panel-body"><?= $model->name?></div>
+                  </div>
+                  <div class="panel panel-default">
+                    <div class="panel-body">
+                          <?= Html::img($model->thumbnail)?>
+                    </div>
+                    <div class="panel-footer">
+                     </div>                      
+                  </div>
+                </div>
+           </div>
+        <?php endforeach; ?>
+    </div>    
+    <div class="row">
+		<?= LinkPager::widget([
+			'pagination' => $pages,
+		]); ?>
+	</div>
+</div>
