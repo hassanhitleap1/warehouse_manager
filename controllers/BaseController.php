2a26a6741750e0f2520cf87e6c305ddc47b1429e
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2020-12-14 11:02:00 +0200

    Create BaseController.php
    cerate BaseController

---
+++ controllers/BaseController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * CategoriesController implements the CRUD actions for Categories model.
+ */
+class BaseController extends Controller
+{
+    public function beforeAction($action)
+    {
+        if (Yii::$app->user->isGuest) {
+            return $this->redirect(['site/login']);
+        }
+        return $action;
+    }
+}
