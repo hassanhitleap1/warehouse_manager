5d8f7de3b152cd5e0b83d9df17f330b5de4bd161
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2020-12-14 12:57:40 +0200

    Update OrderForm.php
    form

--- models/orders/OrderForm.php
+++ models/orders/OrderForm.php
@@ -1,1 +1,27 @@
 <?php
+namespace app\models\orders;
+
+use Yii;
+use yii\base\Model;
+
+class  OrderForm extends Model{
+
+  public $order_id;
+   public $phone;
+   public $name;
+   public $other_phone;
+   public $address;
+   public $product_id;
+   public $sub_product_id;
+   public $amount_required;
+   public $total_price;
+   public $discount=0;
+   public $delivery_price=0;
+    public $address;
+    public $country_id;
+    public $region_id;
+  
+  
+  
+  
+}
