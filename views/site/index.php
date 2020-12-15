d80c5f7c29ca7fcf428fbdde7372655a7a98fe33
hassanhitleap1 <34882574+hassanhitleap1@users.noreply.github.com>
2020-12-15 12:55:12 +0200

    Update index.php
    Url

--- views/site/index.php
+++ views/site/index.php
@@ -11,19 +11,21 @@ use yii\helpers\Html;
     <div class="row">
 		<?php foreach ($models as  $key => $model) : ?>
             <div class="col-md-4">
-              <?= Html::a('Link to abc', ['product/view','id'=> $model->id]) ?>
-                <div class="panel-group">
-                  <div class="panel panel-primary">
-                    <div class="panel-body"><?= $model->name?></div>
-                  </div>
-                  <div class="panel panel-default">
-                    <div class="panel-body">
-                          <?= Html::img($model->thumbnail,['width'=>'400','height'=>'200'])?>
-                    </div>
-                    <div class="panel-footer">
-                     </div>                      
-                  </div>
-                </div>
+             
+		<a href="<?= Url::toRoute(['product/view','id' => $model->id])?>">  
+			<div class="panel-group">
+			  <div class="panel panel-primary">
+			    <div class="panel-body"><?= $model->name?></div>
+			  </div>
+			  <div class="panel panel-default">
+			    <div class="panel-body">
+				  <?= Html::img($model->thumbnail,['width'=>'400','height'=>'200'])?>
+			    </div>
+			    <div class="panel-footer">
+			     </div>                      
+			  </div>
+			</div>
+		    </a>
            </div>
         <?php endforeach; ?>
     </div>    
