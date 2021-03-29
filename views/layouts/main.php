09de82c5b9e35043f11d419d3bdaa23bc1aca33b
hassanhitleap1 <hassanhitleap1@gmail.com>
2021-03-29 09:44:25 -0700

    mm

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -15,6 +15,7 @@ MainAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
+
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,14 +24,16 @@ MainAsset::register($this);
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
+
 <body>
-<?php $this->beginBody() ?>
+    <?php $this->beginBody() ?>
 
-<div class="wrap">
-<?php
+    <div class="wrap">
 
-        $menuItemsleft=[];
-        $menuItems=[];
+        <?php
+
+        $menuItemsleft = [];
+        $menuItems = [];
 
         NavBar::begin([
             //'brandLabel' => Html::img('@web/images/logo.svg'),
@@ -44,51 +47,50 @@ MainAsset::register($this);
 
         if (Yii::$app->user->isGuest) {
             $menuItemsleft[] = ['label' => Yii::t('app', 'Login'), 'url' => ['/site/login']];
-       
         } else {
             $menuItems[] = ['label' => Yii::t('app', 'Dashboard'), 'url' => ['/site/dashboard']];
             $menuItems[] = [
-                'label' =>Yii::t('app', 'Countries') ,
+                'label' => Yii::t('app', 'Countries'),
                 'items' => [
                     ['label' => Yii::t('app', 'Countries'), 'url' => ['/countries/index']],
                     ['label' => Yii::t('app', 'Regions'), 'url' => ['/regions/index']],
                     ['label' => Yii::t('app', 'Area'), 'url' => ['/area/index']],
-                   
+
                 ],
             ];
 
             $menuItems[] = [
-                'label' =>Yii::t('app', 'Additional') ,
+                'label' => Yii::t('app', 'Additional'),
                 'items' => [
                     ['label' => Yii::t('app', 'Categorises'), 'url' => ['/categorises/index']],
                     ['label' => Yii::t('app', 'Units'), 'url' => ['/units/index']],
                     ['label' => Yii::t('app', 'Status'), 'url' => ['/status/index']],
                     ['label' => Yii::t('app', 'Warehouse'), 'url' => ['/warehouse/index']],
-                 
+
                 ],
             ];
 
             $menuItems[] = [
-                'label' =>Yii::t('app', 'Users') ,
+                'label' => Yii::t('app', 'Users'),
                 'items' => [
                     ['label' => Yii::t('app', 'Suppliers'), 'url' => ['/suppliers/index']],
                     ['label' => Yii::t('app', 'Users'), 'url' => ['/users/index']],
-                 
+
                 ],
             ];
 
             $menuItems[] = [
-                'label' =>Yii::t('app', 'Products') ,
+                'label' => Yii::t('app', 'Products'),
                 'items' => [
                     ['label' => Yii::t('app', 'Products'), 'url' => ['/products/index']],
                     ['label' => Yii::t('app', 'SubProductCount'), 'url' => ['/sub-product-count/index']],
                     ['label' => Yii::t('app', 'Type_Options'), 'url' => ['/options-sell-product/index']],
 
-                 
+
                 ],
             ];
 
-           
+
             $menuItems[] = ['label' => Yii::t('app', 'Orders'), 'url' => ['/orders/index']];
 
             $menuItems[] = '<li>'
@@ -111,23 +113,25 @@ MainAsset::register($this);
         NavBar::end();
         ?>
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+        <div class="container">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= Alert::widget() ?>
+            <div class="loader"></div>
+            <?= $content ?>
+        </div>
     </div>
-</div>
 
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy;  <?= Yii::$app->name.' ' .date('Y') ?></p>
+    <footer class="footer">
+        <div class="container">
+            <p class="pull-left">&copy; <?= Yii::$app->name . ' ' . date('Y') ?></p>
 
-    </div>
-</footer>
+        </div>
+    </footer>
 
-<?php $this->endBody() ?>
+    <?php $this->endBody() ?>
 </body>
+
 </html>
 <?php $this->endPage() ?>
