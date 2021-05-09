de2f15fab5bf5b6c233ae09905726e92498263c4
hassan kiwan <hassanhitleap1@gmail.com>
2021-05-09 13:50:33 +0300

    5/5/2021

--- views/products/_form.php
+++ views/products/_form.php
@@ -10,7 +10,11 @@ use kartik\select2\Select2;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\editors\Summernote;
 
+// on your view layout file
+use kartik\icons\FontAwesomeAsset;
+FontAwesomeAsset::register($this);
 
 /* @var $this yii\web\View */
 /* @var $model app\models\products\Products */
@@ -170,8 +174,9 @@ if (!$model->isNewRecord) {
             ?>
             </div>
             <div class="col-md-4">
-                <?= $form->field($model, 'description')->textarea(['rows' => '6']) ?> 
-
+                <?= $form->field($model, 'description')->widget(Summernote::class, [
+                'options' => ['placeholder' => 'Edit your description here...']
+                ]); ?>
             </div>
             
             
