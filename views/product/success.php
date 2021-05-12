dfa874ea5cbf19865a2d04a8853ff4f5741bdfe4
hasan kiwan <hassankiwan92@gmail.com>
2021-05-12 23:32:38 +0300

    mm

--- views/product/success.php
+++ views/product/success.php
@@ -115,3 +115,34 @@ $this->title = Yii::t('app','Successfuly_Applay');
 
 
 
+<script type="text/javascript">
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '{your-pixel-id-goes-here}');
+    fbq('track', 'Purchase', {currency: "USD", value: 10.00});
+</script>
+
+  <!-- Snap Pixel Code -->
+  <script type='text/javascript'>
+        (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+        {a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+        a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+        r.src=n;var u=t.getElementsByTagName(s)[0];
+        u.parentNode.insertBefore(r,u);})(window,document,
+        'https://sc-static.net/scevent.min.js');
+
+        snaptr('init', '6ea08b95-a6d5-43b0-baaf-ec8c8a44a74c', {
+        'user_email': '__INSERT_USER_EMAIL__'
+        });
+
+        snaptr('track','PURCHASE'); 
+
+    </script>
+    <!-- End Snap Pixel Code -->
--- views/product/thanks.php
+++ views/product/thanks.php
@@ -127,3 +127,21 @@ $this->title = Yii::t('app','Successfuly_Applay');
     fbq('track', 'Purchase', {currency: "USD", value: 10.00});
 </script>
 
+  <!-- Snap Pixel Code -->
+  <script type='text/javascript'>
+        (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+        {a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+        a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+        r.src=n;var u=t.getElementsByTagName(s)[0];
+        u.parentNode.insertBefore(r,u);})(window,document,
+        'https://sc-static.net/scevent.min.js');
+
+        snaptr('init', '6ea08b95-a6d5-43b0-baaf-ec8c8a44a74c', {
+        'user_email': '__INSERT_USER_EMAIL__'
+        });
+
+        snaptr('track','PURCHASE'); 
+
+    </script>
+    <!-- End Snap Pixel Code -->
+
