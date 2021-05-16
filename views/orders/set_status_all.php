dd68f76c330353adf5b7dae4451518b9c86a31d1
hassan kiwan <hassanhitleap1@gmail.com>
2021-05-16 17:22:36 +0300

    5/10/2021

---
+++ views/orders/set_status_all.php
@@ -0,0 +1,10 @@
+
+<div class="orders-view">
+        <div class="row">
+            <div class="" align="center">
+                <?php foreach ($status as $statu):?>
+                    <button type="button" class="btn btn-primary btn-lg change-status" att_id="<?=$model->id?>"  att_id_string="<?=$_GET['id_string']?>" att_status_id="<?= $statu->id?>" name_status="<?= $statu["name_ar"]?>"><?= $statu["name_ar"]?></button>
+                <?php endforeach;?>
+            </div>
+        </div>
+</div>
