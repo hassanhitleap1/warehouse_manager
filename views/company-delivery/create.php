c797d97fa91540fafc704ecd4ed13ea531da2652
hasan kiwan <hassankiwan92@gmail.com>
2021-05-19 20:36:34 +0300

    mm

--- views/company-delivery/create.php
+++ views/company-delivery/create.php
@@ -5,8 +5,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\companydelivery\CompanyDelivery */
 
-$this->title = Yii::t('app', 'Create Company Delivery');
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Company Deliveries'), 'url' => ['index']];
+$this->title = Yii::t('app', 'Create_Company_Delivery');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Company_Deliveries'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="company-delivery-create">
