f361a1931a6b30a45188fbe80a3b9ff385f7bed5
hassan kiwan <hassanhitleap1@gmail.com>
2021-05-19 16:47:08 +0300

    6

--- views/settings/index.php
+++ views/settings/index.php
@@ -1,3 +1,34 @@
 <?php
 
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
 ?>
+
+<div class="advertisement-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'adminEmail')->textInput(['maxlength' => true,'value'=>Yii::$app->params['adminEmail']]) ?>
+    <?= $form->field($model, 'senderEmail')->textInput(['maxlength' => true,'value'=>Yii::$app->params['senderEmail']]) ?>
+    <?= $form->field($model, 'senderName')->textInput(['maxlength' => true,'value'=>Yii::$app->params['senderName']]) ?>
+
+
+    <?= $form->field($model, 'bsDependencyEnabled')->textInput(['maxlength' => true,'value'=>Yii::$app->params['bsDependencyEnabled']]) ?>
+
+    <?= $form->field($model, 'phone')->textInput(['maxlength' => true,'value'=>Yii::$app->params['phone']]) ?>
+
+    <?= $form->field($model, 'facebook_id')->textInput(['maxlength' => true,'value'=>Yii::$app->params['facebook_id']]) ?>
+    <?= $form->field($model, 'sanpchat_id')->textInput(['maxlength' => true,'value'=>Yii::$app->params['sanpchat_id']]) ?>
+    <?= $form->field($model, 'sanpchat_email')->textInput(['maxlength' => true,'value'=>Yii::$app->params['sanpchat_email']]) ?>
+    <?= $form->field($model, 'tiktok_id')->textInput(['maxlength' => true,'value'=>Yii::$app->params['tiktok_id']]) ?>
+    <?= $form->field($model, 'company_delivery')->textInput(['maxlength' => true,'value'=>Yii::$app->params['company_delivery']]) ?>
+
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
