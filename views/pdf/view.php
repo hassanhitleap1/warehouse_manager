46993cad5bf93c6133b44f5879406563c5b062df
hassan kiwan <hassanhitleap1@gmail.com>
2021-05-23 16:32:00 +0300

    20

--- views/pdf/view.php
+++ views/pdf/view.php
@@ -1,30 +1,17 @@
 <?php
 
-use Endroid\QrCode\QrCode;
+if (!extension_loaded('imagick')){
+    echo 'imagick not installed';
+}
 
-$qrCode = new QrCode();
-header('Content-Type: '.$qrCode->getContentType());
-// or create a response object
-$response = new Response($qrCode->get(), 200, array('Content-Type' => $qrCode->getContentType()));
-print_r($qrCode->getContentType());
-exit();
-$qrCode
-    ->setText('Life is too short to be generating QR codes')
-    ->setSize(300)
-    ->setPadding(10)
-    ->setErrorCorrection('high')
-    ->setForegroundColor(array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0))
-    ->setBackgroundColor(array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0))
-    ->setLabel('Scan the code')
-    ->setLabelFontSize(16)
-    ->setImageType(QrCode::IMAGE_TYPE_PNG)
-;
-print_r($qrCode);
-exit();
+use BaconQrCode\Renderer\ImageRenderer;
+use BaconQrCode\Renderer\Image\ImagickImageBackEnd;
+use BaconQrCode\Renderer\RendererStyle\RendererStyle;
+use BaconQrCode\Writer;
 
-// now we can directly output the qrcode
-header('Content-Type: '.$qrCode->getContentType());
-$qrCode->render();
-
-// or create a response object
-$response = new Response($qrCode->get(), 200, array('Content-Type' => $qrCode->getContentType()));
+$renderer = new ImageRenderer(
+    new RendererStyle(400),
+    new ImagickImageBackEnd()
+);
+$writer = new Writer($renderer);
+$writer->writeFile('Hello World!', 'qrcode.png');
