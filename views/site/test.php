4db757b77654fc463ab31e36875eda0f25d177d1
hassan kiwan <hassanhitleap1@gmail.com>
2021-06-01 12:41:27 +0300

    34

--- views/site/test.php
+++ views/site/test.php
@@ -1,5 +1,6 @@
 <?php
 $data = 'otpauth://totp/test?secret=B3JX4VCVJDVNXNZ5&issuer=chillerlan.net';
+$data = 'chillerlan.net';
 
 // quick and simple:
 echo '<img src="'.(new \chillerlan\QRCode\QRCode())->render($data).'" alt="QR Code" />';
