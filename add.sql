7cb0865461fc3e60956ff489d79560fb8bd5de26
hassan kiwan <hassanhitleap1@gmail.com>
2021-06-07 17:55:46 +0300

    25

--- add.sql
+++ add.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `products` ADD `quantity_come` INT NOT NULL DEFAULT '0' AFTER `quantity`;
 UPDATE `products` SET `products`.`quantity_come`= `products`.`quantity` WHERE 1
+ALTER TABLE `products` CHANGE `description` `description` LONGTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
