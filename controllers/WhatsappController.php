372443702fd9b0cf205a56651ca1431613a356dd
hassan kiwan <hassanhitleap1@gmail.com>
2021-06-09 18:46:50 +0300

    26

--- controllers/WhatsappController.php
+++ controllers/WhatsappController.php
@@ -5,9 +5,10 @@ namespace app\controllers;
 
 
 use app\models\orders\Orders;
-use app\models\ordersitem\OrdersItem;
+use Yii;
+use yii\web\Controller;
 
-class WhatsappController
+class WhatsappController extends Controller
 {
         public function actionGetMassges(){
             \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
@@ -16,7 +17,6 @@ class WhatsappController
             $data=[];
             $orders=Orders::find()->where(['in','id',$ides])->all();
             foreach ($orders as $key => $order){
-
                 $orderItemString = '';
                 foreach ($order->orderItems as $orderItem) {
                     $type = '';
@@ -26,12 +26,14 @@ class WhatsappController
                     $orderItemString .= ' ' . $orderItem['product']['name'] . ' ' . $type . ' ' . Yii::t('app', 'Number') . ' ( ' . $orderItem->quantity . ' ) </br>';
                 }
 
-                $data[$key]=[
+                $data[]=[
                     'order_str'=>$orderItemString,
                     'phone'=> $order['user']['phone'],
                     'name'=>$order['user']['name'],
                 ];
+
             }
+
             return ['code'=>201,'data'=>$data];
         }
 }
