44aac4865648b190b86289684e2f673bd87cacdf
hassan kiwan <hassanhitleap1@gmail.com>
2021-06-13 18:39:13 +0300

    70

--- controllers/SettingsController.php
+++ controllers/SettingsController.php
@@ -38,6 +38,7 @@ class SettingsController extends BaseController
         $model = new Settings();
         $params=Yii::$app->params;
         if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+
             $params['adminEmail'] = $model->adminEmail;
             $params['senderEmail'] = $model->senderEmail;
             $params['senderName'] = $model->senderName;
@@ -48,8 +49,11 @@ class SettingsController extends BaseController
             $params['sanpchat_email'] = $model->sanpchat_email;
             $params['tiktok_id'] = $model->tiktok_id;
             $params['company_delivery'] = $model->company_delivery;
-            $param['massage_whatsapp']= $model->massage_whatsapp;
-            $param['name_of_store']= $model->name_of_store;
+            $params['massage_whatsapp']= $model->massage_whatsapp;
+            $params['name_of_store']= $model->name_of_store;
+
+
+
             $file = UploadedFile::getInstance($model, 'logo');
             if (!is_null($file)) {
                 $path="images/logo" . "." . $file->extension;
