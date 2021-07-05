cfceedac1936b318790f5b337fe0292265dcf66b
hasan kiwan <hassankiwan92@gmail.com>
2021-07-06 01:08:50 +0300

    mm

--- views/dashboard/best-seller.php
+++ views/dashboard/best-seller.php
@@ -4,117 +4,40 @@
 
 use yii\helpers\Html;
 
-$this->title = Yii::t('app','Dashboard');
+
+$this->title = Yii::t('app', 'The_Sales');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app','Dashboard'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 
-
-//$label_month;
-// $label_day;
-// $orders_count_month;
-// $orders_count_day; 
-// $profits_month;
 
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-    <div class="row">
-        <div class="col-md-6">
-        <canvas id="profits_day"></canvas>
-        </div>
-        <div class="col-md-6">
-    
-            <canvas id="order_day"></canvas>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-6">
-        <canvas id="profits_month"></canvas>
-        </div>
-        <div class="col-md-6">
-    
-            <canvas id="order_month"></canvas>
-        </div>
-    </div>
-    
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-    <script  type="text/javascript">
-        var canvas_profits_day = document.getElementById('profits_day').getContext('2d');
-        var canvas_order_day = document.getElementById('order_day').getContext('2d');
-        var canvas_profits_month = document.getElementById('profits_month').getContext('2d');
-        var canvas_order_month = document.getElementById('order_month').getContext('2d');
+   
+    <table class="table table-striped">
+    <thead>
+      <tr>
+        <th><?= Yii::t('app','Product');?></th>
+        <th><?= Yii::t('app','Type');?></th>
+        <th><?= Yii::t('app','Count');?></th>
+        <th><?= Yii::t('app','The_Sales');?></th>
+        <th><?= Yii::t('app','Profit_Margin');?></th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php foreach($orders as $order):?>
+      <tr>
+        <th><?= $order["name"]?></th>
+        <th><?= $order["type"]?></th>
+        <th><?= $order["count_quantity"]?></th>
+        <th><?= $order["total_sales"]?> JD</th>
+        <th><?= $order["profits_margin"]?> JD</th>
       
-        var label_day = <?php echo json_encode($label_day); ?>;
-        var label_month = <?php echo json_encode($label_month); ?>;
-        
-        var profits_day = <?php echo json_encode($profits_day); ?>;
-        var orders_count_day = <?php echo json_encode($orders_count_day); ?>;
-        var profits_month = <?php echo json_encode($profits_month); ?>;
-        var orders_count_month = <?php echo json_encode($orders_count_month); ?>;
-     
-        let data_profits_day= {
-                labels:  label_day,
-                datasets: [{
-                    label: 'ارباح اليومية',
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: profits_day
-                }]
-            };
-
-        let data_orders_day= {
-                labels:  label_day,
-                datasets: [{
-                    label: 'الطلبات اليومية',
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: orders_count_day
-                }]
-            };    
-
-
-        let data_profits_month= {
-                labels:  label_month,
-                datasets: [{
-                    label: 'ارباح الشهرية',
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: profits_month
-                }]
-            };
-
-           
-        let data_orders_month= {
-                labels:  label_month,
-                datasets: [{
-                    label: 'الطلبات الشهرية',
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: orders_count_month
-                }]
-            };     
-
-
+      </tr>
+      <tr>
+     <?php endforeach;?> 
       
-        var chart_profits_day = new Chart(canvas_profits_day, {
-            type: 'line',
-            data:data_profits_day,
-            options: {}
-        });
-        var chart_order_day = new Chart(canvas_order_day, {
-            type: 'bar',
-            data:data_profits_month,
-            options: {}
-        });
-        var chart_profits_month= new Chart(canvas_profits_month, {
-            type: 'line',
-            data:data_profits_month,
-            options: {}
-        });
-        var chart_order_month = new Chart(canvas_order_month, {
-            type: 'bar',
-            data:data_orders_month,
-            options: {}
-        });
-    </script>
+    </tbody>
+  </table>
 
 </div>
--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -17,33 +17,38 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <div class="row" >
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="panel panel-default">
-                <div class="panel-body">عدد طلبات اليوم</div>
+                <div class="panel-body"> <?= $orders["count_order"]?>  عدد طلبات اليوم</div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="panel panel-default">
-                <div class="panel-body">عدد القطع</div>
+                <div class="panel-body"> <?= $orders["total_sales"]?>  مبيعات اليوم</div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="panel panel-default">
-                <div class="panel-body">مربح اليوم</div>
+                <div class="panel-body"> <?= $orders["quantity"]?>  عدد القطع</div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body"> <?= $orders["profit_margin"]?>  مربح اليوم</div>
             </div>
         </div>
 
     </div>
     <div class="row" >
 
-            <div class="col-md-4">
+            <div class="col-md-3">
                 <div class="card" onclick="window.location.href = '<?= 'index.php?r=dashboard/sales'?>'">
                     <?= Html::img("/images/icons/sales.svg", ['style' => 'width:100%']) ?>
                     <h1 style="padding: 12px;"><?= Yii::t('app','The_Sales') ?></h1>
 
                 </div>
             </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="card" onclick="window.location.href = '<?= 'index.php?r=dashboard/outlay'?>'">
                 <?= Html::img("/images/icons/outlay.svg", ['style' => 'width:100%']) ?>
                 <h1 style="padding: 12px;"><?= Yii::t('app','The_Outlay') ?></h1>
@@ -51,7 +56,7 @@ $this->params['breadcrumbs'][] = $this->title;
             </div>
         </div>
 
-        <div class="col-md-4">
+        <div class="col-md-3">
             <div class="card" onclick="window.location.href = '<?= 'index.php?r=dashboard/best-seller'?>'">
                 <?= Html::img("/images/icons/best-seller.svg", ['style' => 'width:100%']) ?>
                 <h1 style="padding: 12px;"><?= Yii::t('app','Best_Seller') ?></h1>
@@ -59,7 +64,7 @@ $this->params['breadcrumbs'][] = $this->title;
             </div>
         </div>
 
-            <div class="col-md-4">
+            <div class="col-md-3">
             <div class="card" onclick="window.location.href = '<?= 'index.php?r=dashboard/orders'?>'">
                 <?= Html::img("/images/icons/order.svg", ['style' => 'width:100%']) ?>
                 <h1 style="padding: 12px;" ><?= Yii::t('app','The_Orders') ?></h1>
