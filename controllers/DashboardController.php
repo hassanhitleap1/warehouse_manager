7716cc681f10399a237256d07895785d16f6b31c
hassan kiwan <hassanhitleap1@gmail.com>
2021-07-06 15:35:51 +0300

    18

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -60,53 +60,33 @@ class  DashboardController extends BaseController {
         }else{
             $day = date('d', strtotime(date('Y-m-d'). ' -7 day'));
         }
-
-    
         $profits_day_model = Orders::find()->select([
              'count(*) as count_order',
              'sum(profit_margin)  as profit_margin',
              'MONTH(`created_at`) as month',
+             'count(quantity) as quantity',
              'DAY(`created_at`) as day'
-             ])
-            ->andWhere('YEAR(created_at)=:year', [':year' => date('Y')])
+             ])->andWhere('YEAR(created_at)=:year', [':year' => date('Y')])
             ->andWhere('MONTH(created_at)=:month', [':month' => date('m')])
-            //->andWhere('DAY(created_at) >= :day', [':day' => $day])
-            ->groupBy(['YEAR(`created_at`)', ' MONTH(`created_at`)','DAY(`created_at`)'])
+            ->groupBy(['DAY(`created_at`)'])
             ->asArray()->all();
-        // print_r($profits_day_model);
-        // exit;
-        $profits_month_model = Orders::find()->select(['count(*) as count_order', 'sum(profit_margin)  as profit_margin','MONTH(`created_at`) as month'])
-            ->andWhere('YEAR(created_at)=:year', [':year' => date('Y')])
-            ->andWhere('MONTH(created_at)>= :month', [':month' => $month])
-            ->groupBy(['YEAR(`created_at`)', ' MONTH(`created_at`)'])
+        $profits_month_model = Orders::find()->select([
+            'count(*) as count_order',
+            'sum(profit_margin)  as profit_margin',
+            'MONTH(`created_at`) as month',
+            'count(quantity) as quantity'
+        ])->andWhere('YEAR(created_at)=:year', [':year' => date('Y')])
+            ->groupBy(['MONTH(`created_at`)'])
             ->asArray()->all();
 
-        $label_month=[];
-        $label_day=[];
-        $orders_count_month=[];
-        $orders_count_day=[];
-        $profits_month=[];
-        $profits_day=[];
-
-        foreach($profits_day_model as $profit_day){
-            $label_day[]=$profit_day['day'];
-            $orders_count_day[]=$profit_day['count_order'];
-            $profits_day[]=$profit_day['profit_margin'];
-        }
 
 
-        foreach($profits_month_model as $profit_month){
-            $label_month[]=$profit_day['month'];
-            $orders_count_month[]=$profit_day['count_order'];
-            $profits_month[]=$profit_day['profit_margin'];
-        }
+
+
+
         return $this->render('sales',[
-            'label_month'=>$label_month,
-            'label_day'=>$label_day,
-            'orders_count_month'=>$orders_count_month,
-            'orders_count_day'=>$orders_count_day,
-            'profits_month'=>$profits_month,
-            'profits_day'=> $profits_day,
+            'profits_day_model'=>$profits_day_model,
+            'profits_month_model'=>$profits_month_model,
         ]);
     }
 
