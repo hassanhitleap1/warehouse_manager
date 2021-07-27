64d0c1cfb856f095f0e79d818c84098231c4c1d0
hassan kiwan <hassanhitleap1@gmail.com>
2021-07-27 17:27:37 +0300

    14

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -192,35 +192,25 @@ class  DashboardController extends BaseController {
     }
 
     public function actionOrders(){
+        $date_day = date('Y-m-d', strtotime(date('Y-m-d'). ' -7 day'));
+        $date_month = date('Y-m-d', strtotime(date('Y-m-d'). ' -7 month'));
 
-        if(date('m') < 7 ){
-            $month=date('m');
-        }else{
-            $month = date('m', strtotime(date('Y-m-d'). ' -7 month'));
-        }
-
-        if(date('d') < 7 ){
-            $day=date('d');
-        }else{
-            $day = date('d', strtotime(date('Y-m-d'). ' -7 day'));
-        }
-
-        // SELECT count(*) ,count(`profit_margin`) FROM `orders` GROUP BY YEAR(`created_at`), MONTH(`created_at`),DAY(`created_at`)
         $profits_day_model = Orders::find()->select([
             'count(*) as count_order', 
-            'sum(profit_margin)  as profit_margin',
-            'MONTH(`created_at`) as month',
-            'DAY(`created_at`) as day'])
-            ->andWhere('YEAR(created_at)=:year', [':year' => date('Y')])
-            ->andWhere('MONTH(created_at)=:month', [':month' => date('m')])
-            ->andWhere('DAY(created_at) >= :day', [':day' => $day])
-            ->groupBy(['YEAR(`created_at`)', ' MONTH(`created_at`)','DAY(`created_at`)'])
-            ->asArray()->all();
+            '(SELECT SUM(orders_item.profits_margin) as profits_margin  FROM `orders_item` inner join orders as ord on ord.id = orders_item.order_id where 
+               ord.status_id not in (6,7,8,9,10,11,13)  and date(orders_item.created_at) >= "'.$date_day.'")',
+            'date(`created_at`) as date'])
+            ->andWhere('date(created_at) >= :date', [':date' => $date_day])
+            ->groupBy(['DAY(`created_at`)'])
+         ->asArray()->all();
 
-        $profits_month_model = Orders::find()->select(['count(*) as count_order', 'sum(profit_margin)  as profit_margin','MONTH(`created_at`) as month'])
-            ->andWhere('YEAR(created_at)=:year', [':year' => date('Y')])
-            ->andWhere('MONTH(created_at)>= :month', [':month' => $month])
-            ->groupBy(['YEAR(`created_at`)', ' MONTH(`created_at`)'])
+        $profits_month_model = Orders::find()->select([
+            'count(*) as count_order',
+            '(SELECT SUM(orders_item.profits_margin) as profits_margin  FROM `orders_item` inner join orders as ord on ord.id = orders_item.order_id where 
+               ord.status_id not in (6,7,8,9,10,11,13)  and date(orders_item.created_at) >= "'.$date_day.'")',
+            'MONTH(`created_at`) as month'])
+            ->andWhere('date(created_at) >= :date', [':date' => $date_day])
+            ->groupBy(['MONTH(`created_at`)'])
             ->asArray()->all();
 
         $label_month=[];
