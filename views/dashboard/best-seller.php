eadc0e4f21d028fa6c82db97dddace4bc6f966b4
hassan kiwan <hassanhitleap1@gmail.com>
2021-07-27 16:16:47 +0300

    13

--- views/dashboard/best-seller.php
+++ views/dashboard/best-seller.php
@@ -48,8 +48,9 @@ $data=[];
     </table>
    </div>
    <div class="col-md-6">
- 
+
      <canvas id="chart"></canvas>
+
 
    </div>
 
@@ -58,52 +59,27 @@ $data=[];
 
 <script>
 
-var label = <?=json_encode($label)?>;
-var  data = {
-  labels: label,
-  datasets: [{
-    label: 'الارباح ',
-    backgroundColor: 'rgb(255, 99, 132)',
-    borderColor: 'rgb(255, 99, 132)',
-    data: <?=json_encode($data)?> ,
-  }]
-};
-var config = {
-  type: 'doughnut',
-  data: data,
-  backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-  options: {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Doughnut Chart'
-      }
-    }
-  },
-};
+    var ctx = document.getElementById('chart').getContext('2d');
+     data = {
+        labels: <?=json_encode($label)?>,
+        datasets: [{
+            label: 'الاكثر مبيعا',
+            data: <?=json_encode($data)?> ,
+            borderWidth: 1,
+            backgroundColor: ['#CB4335', '#1F618D', '#F1C40F', '#27AE60', '#884EA0', '#D35400', '#57AE60', '#894EA0', '#D37400','#27AE60', '#884EA0', '#D35400'],
+        }]
+    };
 
-new Chart(
-    document.getElementById('chart'),
-    config
-  );
+    var config = {
+        type: 'pie',
+        data: data,
+        options: {}
+    };
+
+    new Chart(
+        document.getElementById('chart'),
+        config
+    );
+
 
 </script>  
