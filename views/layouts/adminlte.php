0daade9adf35b6eee4bf7e52b2565ef3ce178849
hassan kiwan <hassanhitleap1@gmail.com>
2021-08-04 09:39:44 +0300

    95

--- views/layouts/adminlte.php
+++ views/layouts/adminlte.php
@@ -62,7 +62,7 @@ AdminLteAsset::register($this);
     </script>
     <!-- TikTok Pixel Code End -->
 
-    
+
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
