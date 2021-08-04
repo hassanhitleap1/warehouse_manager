c94f1760973ff05eeb91ee7d9818a4b84bb16de7
hassan kiwan <hassanhitleap1@gmail.com>
2021-08-04 09:28:16 +0300

    add theme

---
+++ assets/AdminLteAsset.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main application asset bundle.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class  AdminLteAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css ;
+    public $js ;
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'airani\bootstrap\BootstrapRtlAsset',
+    ];
+
+
+    function __construct() {
+        $path_theme="theme/adminlte-rtl";
+        $this->css=[
+            "css/site.css?v=".rand(),
+            'css/orders.css?v='.rand(),
+            'css/swiper/swiper-bundle.min.css',
+
+        ];
+        $this->js=[
+            'js/main.js?v='.rand(),
+            'js/custum.js?v='.rand(),
+            'js/sweetalert2/main.js',
+        ];
+    }
+}
