3ab68940a9d01dda25af44c6c67fc39e64ec13a4
hassan kiwan <hassanhitleap1@gmail.com>
2021-08-11 17:19:52 +0300

    66

---
+++ components/ApiCompanyDriverHelper.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace  app\components;
+
+use Yii;
+use yii\base\BaseObject;
+
+
+class ApiCompanyDriverHelper extends BaseObject
+{
+
+
+
+   public  function get_villages($page=1){
+
+       $request_headers = array(
+           "Authorization-Token:" . "7be004ba6ae88fb4097ef885fdf5fe148886fd68b424b7192a0e060a58e10",
+           "company-d:" . "143"
+       );
+
+       $ch = curl_init("https://apis.logestechs.com/saas/api/addresses/villages?page=$page");
+       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+       curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
+
+       $response = curl_exec($ch);
+       curl_close($ch);
+       return $response;
+
+
+       }
+
+
+    public  function get_regions($page=1){
+        $request_headers = array(
+            "Authorization-Token:" . "7be004ba6ae88fb4097ef885fdf5fe148886fd68b424b7192a0e060a58e10",
+            "company-d:" . "143"
+        );
+
+        $ch = curl_init("https://apis.logestechs.com/saas/api/addresses/regions?page=$page");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+
+
+    }
+
+
+    public  function get_cities($page=1){
+
+        $request_headers = array(
+            "Authorization-Token:" . "7be004ba6ae88fb4097ef885fdf5fe148886fd68b424b7192a0e060a58e10",
+            "company-d:" . "143"
+        );
+
+        $ch = curl_init("https://apis.logestechs.com/saas/api/addresses/cities?page=$page");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+
+    }
+
+
+}
