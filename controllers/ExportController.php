d9938922db1d18f6f89d38ead1c9f0b81f78f24e
hassan kiwan <hassanhitleap1@gmail.com>
2021-08-11 12:46:10 +0300

    33

--- controllers/ExportController.php
+++ controllers/ExportController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 
+use app\models\orders\Orders;
 use app\models\orders\OrdersSearch;
 use Yii;
 use yii\filters\VerbFilter;
@@ -30,12 +31,29 @@ class ExportController extends BaseController
     }
 
     public function actionExport(){
-        $searchModel = new OrdersSearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
-        return $this->render('export', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
-        ]);
+        $string_id=$_GET['string_id'];
+        $ides = explode(",", $string_id);
+        $models=Orders::find()->where(['in','id',$ides])->all();
+        $out = fopen("php://output", 'w');
+        foreach($models as $key =>$model){
+            $data=[
+                "order_id"=>$model['order_id'],
+                "name"=>$model['user']['name'],
+                'phone'=>$model['user']['phone'] ,
+                "region"=>$model['region']['name_ar'],
+                "area"=>$model['region']['name_ar'],
+                "smaplearea"=>$model['region']['name_ar'],
+                'address'=>$model['user']['address'] ,
+                'amount'=>$model['total_price'],
+            ];
+            fputcsv($out, $data,"\t");
+        }
+      
 
+        header("Content-Disposition: attachment; filename=\"demo.xls\"");
+        header("Content-Type: application/vnd.ms-excel;");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        fclose($out);
     }
 }
