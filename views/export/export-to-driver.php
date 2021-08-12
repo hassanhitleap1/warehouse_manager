17f00f3df9435182d3a6296e67a00e30d3b28468
hassan kiwan <hassanhitleap1@gmail.com>
2021-08-12 11:01:45 +0300

    31

--- views/export/export-to-driver.php
+++ views/export/export-to-driver.php
@@ -17,52 +17,71 @@ $api= new ApiOrderHelper();
 <div class="Create_Product">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
-
+    <button id='export_to_driver' class='btn btn-success' title="<?= Yii::t('app', 'Export_To_Driver') ?>" > <span   class='glyphicon glyphicon-plane' > </span> </button>
     <table class="table table-sm">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col"><?= Yii::t('app','Name')?></th>
-      <th scope="col"><?= Yii::t('app','Phone')?></th>
-      <th scope="col"><?= Yii::t('app','Address')?></th>
-      <th scope="col"><?= Yii::t('app','Region')?></th>
-      <th scope="col"><?= Yii::t('app','Total_Price')?></th>
-        <th scope="col"><?= Yii::t('app','Action')?></th>
-    </tr>
-  </thead>
-  <tbody>
-      <?php foreach($models as $key =>$model):?>
-        <?php
-        if($model->deported == Orders::UN_DEPOTED){
-            $responce=$api->push_order($model);
-            if(isset($responce["error"])){
-                $class_name="bg-danger";
+      <thead>
+        <tr>
+          <th scope="col">
+              #
+                <input type="checkbox" class="kv-row-checkbox" name="selection" >
+          </th>
+          <th scope="col"><?= Yii::t('app','Name')?></th>
+          <th scope="col"><?= Yii::t('app','Phone')?></th>
+          <th scope="col"><?= Yii::t('app','Address')?></th>
+          <th scope="col"><?= Yii::t('app','Region')?></th>
+          <th scope="col"><?= Yii::t('app','Total_Price')?></th>
+            <th scope="col"><?= Yii::t('app','Action')?></th>
+        </tr>
+      </thead>
+      <tbody>
+            <?php
+            $total_exprot=0;
+            $max_exprt=20
+            ?>
+          <?php foreach($models as $key =>$model):?>
+            <?php
+            if($model->deported == Orders::UN_DEPOTED ){
+                if($total_exprot < $max_exprt){
+                    $responce=$api->push_order($model);
+                    if(isset($responce["error"])){
+                        $class_name="bg-danger";
+                    }else{
+                        $total_exprot ++;
+                        Yii::$app->db->createCommand()
+                            ->update('orders', ['order_id' => $responce['barcode'],'deported'=>Orders::DEPOTED], "orders.id =". $model['id'])
+                            ->execute();
+                        $class_name="bg-success";
+                    }
+                }else{
+                    $class_name="bg-danger";
+                }
             }else{
-                Yii::$app->db->createCommand()
-                    ->update('orders', ['order_id' => $responce['barcode'],'deported'=>Orders::DEPOTED], "orders.id =". $model['id'])
-                    ->execute();
                 $class_name="bg-success";
             }
-        }else{
-            $class_name="bg-success";
-        }
-        ?>
-        <tr class="<?=$class_name?>">
-            <th scope="row"><?= ++$key?></th>
-            <td><?= $model["name"] ?></td>
-            <td><?= $model["phone"] ?></td>
-            <td><?= $model["address"] ?></td>
-            <td><?= $model['name_ar']?></td>
-            <td><?= $model["total_price"] ?> </td>
-            <td> <? Html::a("<span   class='glyphicon glyphicon-plane' > </span>",
-                    ['export/export-to-driver', 'string_id'=>$model["id"]],
-                    ['class' => 'btn btn-success' ,"target"=>"_blank"])?>
-            </td>
-        </tr>
-    
-    <?php endforeach;?>
+            ?>
+            <tr class="<?=$class_name?>">
+                <th scope="row">
+                    <?php if($class_name =="bg-danger"):?>
+                        <input type="checkbox" class="kv-row-checkbox" name="selection[]" value="<?=$model["id"]?>">
+                    <?php endif;?>
+                    <?= ++$key?>
+                </th>
+                <td><?= $model["name"] ?></td>
+                <td><?= $model["phone"] ?></td>
+                <td><?= $model["address"] ?></td>
+                <td><?= $model['name_ar']?></td>
+                <td><?= $model["total_price"] ?> </td>
+                <td>
+                    <?php if($class_name =="bg-danger"):?>
+                        <?=Html::a("<span   class='glyphicon glyphicon-plane' > </span>",
+                            ['export/export-to-driver', 'string_id'=>$model["id"]],
+                            ['class' => 'btn btn-success' ,"target"=>"_blank"])?>
+                    <?php endif;?>
+                </td>
+            </tr>
 
-  </tbody>
-</table>
+        <?php endforeach;?>
+
+      </tbody>
+    </table>
 </div>
