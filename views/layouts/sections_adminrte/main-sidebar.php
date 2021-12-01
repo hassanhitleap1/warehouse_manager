3c746433c78099e9b20a2dce1c260fc6053974dd
hasan kiwan <hassanhitleap2@gmail.com>
2021-12-01 14:28:08 +0200

    12-1-2021 media

--- views/layouts/sections_adminrte/main-sidebar.php
+++ views/layouts/sections_adminrte/main-sidebar.php
@@ -4,10 +4,10 @@ use yii\helpers\Html;
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+    <a  class="brand-link">
+        <img src="<?php echo  Yii::$app->request->baseUrl;?>/theme/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
              style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light"><?= Yii::$app->user->identity->name?> </span>
     </a>
 
     <!-- Sidebar -->
@@ -15,10 +15,10 @@ use yii\helpers\Html;
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                <img src="<?php echo  Yii::$app->request->baseUrl;?>/theme/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block">><?= Yii::$app->user->identity->name?></a>
             </div>
         </div>
 
@@ -31,7 +31,7 @@ use yii\helpers\Html;
                     <a href="#" class="nav-link active">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
-                            Dashboard
+                            <?=Yii::t('app','Dashboard')?>
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
@@ -59,10 +59,18 @@ use yii\helpers\Html;
                                 <p>Dashboard v1</p>
                             </a>
                         </li>
+
+                        <li class="nav-item">
+                            <?= Html::a('<i class="far fa-circle nav-icon"> 
+                                <p> '.Yii::t('app','Dashboard').'</p>
+                                </i>',
+                                ['dashboard/orders', ['class'=>"nav-link"]] )
+                            ?>
+                        </li>
                         <li class="nav-item">
                             <a href="./index2.html" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Dashboard v2</p>
+                                <p> <?=Yii::t('app','Dashboard')?></p>
                             </a>
                         </li>
                         <li class="nav-item">
