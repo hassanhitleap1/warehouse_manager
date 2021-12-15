183633815e157db94786ef51ebed33b0a6d646fc
hasan kiwan <hassankiwan92@gmail.com>
2021-12-16 00:31:52 +0200

    settings

--- views/product/thanks.php
+++ views/product/thanks.php
@@ -4,113 +4,25 @@ use yii\helpers\Html;
 $this->title = Yii::t('app','Successfuly_Applay');
 
 ?>
-<div class="row">
-<p>
-<h1>
-<?= Yii::t('app','Success_For_Name') . ' '. $model->name;?>
-</h1>
-</p>
-<p>
-</div>
-<div class="row">
-<h1>
-<?= Yii::t('app','Order_Price_Total') . ' '. $model->total_price ." JOD";?>
-</h1>
-</p>
-</div>
-<div class="row">
-<p>
-<h1>
+<div class="container">
+  
 
-<?= Yii::t('app','Order_Conected') ?>
-</h1>
-</p>
-</div>
-<div class="row">
-<p>
-<h1>
-<?= Yii::t('app','See_Other_Product')?>
-</h1>
-</p>
+            <p  class="thanck-continaer">
+               
+                    <?= Yii::t('app','Success_For_Name') . ' '. $model->name;?> <br />
+                
+                    <?= Yii::t('app','Order_Price_Total') . ' '. $model->total_price ." JOD";?><br />
+                
+                    <?= Yii::t('app','Order_Conected') ?><br />
+                
+                    <?= Yii::t('app','See_Other_Product')?><br />
+               
+            </p>
+ 
+            
+   
 </div>
 
-
-<div class="row">
-            <div class="row">
-                <div class="col-md-9">
-                    <h5> <?=Yii::t('app','Product_Suggested');?></h5>
-                </div>
-                <div class="col-md-3">
-                    <!-- Controls -->
-                    <div class="controls pull-right ">
-                        <a class="left fa fa-chevron-left btn btn-success" href="#carousel-example"
-                           data-slide="prev"></a><a class="right fa fa-chevron-right btn btn-success" href="#carousel-example"
-                                                    data-slide="next"></a>
-                    </div>
-                </div>
-            </div>
-            <div id="carousel-example" class="carousel slide" data-ride="carousel">
-                <!-- Wrapper for slides -->
-                <div class="carousel-inner">
-                    <div class="item active">
-                        <div class="row">
-                            <?php if(isset($product_suggested[0])):?>
-                                <div class="col-sm-6 col-xs-6">
-                                    <div class="card card-sugested" onclick="window.location.href = '<?= 'index.php?r=product/view&id='.$product_suggested[0]->id?>'">
-                                        <?= Html::img($product_suggested[0]->thumbnail, ['style' => 'width:100%']) ?>
-                                        <h5><?= $product_suggested[0]->name ?></h5>
-                                        
-                                        <p><?= $product_suggested[0]->description ?>.</p>
-                                        <p><?= Html::a(Yii::t('app', 'More_Info') . ' <span class="glyphicon glyphicon-eye-open" ></span>', ['product/view', 'id' => $product_suggested[0]->id], ['class' => 'btn  btn-green']); ?></p>
-                                    </div>
-                                </div>
-                            <?php endif;?>
-
-                            <?php if(isset($product_suggested[1])):?>
-                                <div class="col-sm-6 col-xs-6">
-                                    <div class="card card-sugested" onclick="window.location.href = '<?= 'index.php?r=product/view&id='.$product_suggested[1]->id?>'">
-                                        <?= Html::img($product_suggested[1]->thumbnail, ['style' => 'width:100%']) ?>
-                                        <h5><?= $product_suggested[1]->name ?></h5>
-                                     
-                                        <p><?= $product_suggested[1]->description ?>.</p>
-                                        <p><?= Html::a(Yii::t('app', 'More_Info') . ' <span class="glyphicon glyphicon-eye-open" ></span>', ['product/view', 'id' => $product_suggested[1]->id], ['class' => 'btn  btn-green']); ?></p>
-                                    </div>
-                                </div>
-                            <?php endif;?>
-
-                        </div>
-                    </div>
-
-                    <div class="item">
-                        <div class="row">
-                            <?php if(isset($product_suggested[2])):?>
-                                <div class="col-sm-6 col-xs-6">
-                                    <div class="card card-sugested" onclick="window.location.href = '<?= 'index.php?r=product/view&id='.$product_suggested[2]->id?>'">
-                                        <?= Html::img($product_suggested[2]->thumbnail, ['style' => 'width:100%']) ?>
-                                        <h5><?= $product_suggested[2]->name ?></h5>
-                                       
-                                        <p><?= $product_suggested[2]->description ?>.</p>
-                                        <p><?= Html::a(Yii::t('app', 'More_Info') . ' <span class="glyphicon glyphicon-eye-open" ></span>', ['product/view', 'id' => $product_suggested[2]->id], ['class' => 'btn  btn-green']); ?></p>
-                                    </div>
-                                </div>
-                            <?php endif;?>
-
-                            <?php if(isset($product_suggested[3])):?>
-                                <div class="col-sm-6 col-xs-6">
-                                    <div class="card card-sugested" onclick="window.location.href = '<?= 'index.php?r=product/view&id='.$product_suggested[3]->id?>'">
-                                        <?= Html::img($product_suggested[3]->thumbnail, ['style' => 'width:100%']) ?>
-                                        <h5><?= $product_suggested[3]->name ?></h5>
-                                       
-                                        <p><?= $product_suggested[3]->description ?>.</p>
-                                        <p><?= Html::a(Yii::t('app', 'More_Info') . ' <span class="glyphicon glyphicon-eye-open" ></span>', ['product/view', 'id' => $product_suggested[3]->id], ['class' => 'btn  btn-green']); ?></p>
-                                    </div>
-                                </div>
-                            <?php endif;?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
 
 
 <script type="text/javascript">
@@ -125,13 +37,7 @@ $this->title = Yii::t('app','Successfuly_Applay');
 
     fbq('init', '<?=Yii::$app->params['facebook_id']?>');
     fbq('track', 'Purchase', {currency: "USD", value: 10.00});
+
+    snaptr('track','PURCHASE'); 
 </script>
-
-  <!-- Snap Pixel Code -->
-  <script type='text/javascript'>
-
-        snaptr('track','PURCHASE'); 
-
-    </script>
-    <!-- End Snap Pixel Code -->
 
