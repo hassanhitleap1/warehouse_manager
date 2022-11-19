f6e5ea707b76964085390faa17c770e1a062c8d7
hasan kiwan <hasankiwan.action@gmail.com>
2022-11-19 16:31:57 +0300

    order

--- views/layouts/app/footer.php
+++ views/layouts/app/footer.php
@@ -2,7 +2,7 @@
   <footer class="footer">
       <div class="container">
           <div class="row">
-              <div class="col-lg-3 col-md-6 col-sm-6">
+              <div class="col-lg-6 col-md-6 col-sm-6">
                   <div class="footer__about">
                       <div class="footer__logo">
                           <a href="#"><img src="img/footer-logo.png" alt=""></a>
@@ -11,29 +11,8 @@
                       <a href="#"><img src="img/payment.png" alt=""></a>
                   </div>
               </div>
-              <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-6">
-                  <div class="footer__widget">
-                      <h6>Shopping</h6>
-                      <ul>
-                          <li><a href="#">Clothing Store</a></li>
-                          <li><a href="#">Trending Shoes</a></li>
-                          <li><a href="#">Accessories</a></li>
-                          <li><a href="#">Sale</a></li>
-                      </ul>
-                  </div>
-              </div>
-              <div class="col-lg-2 col-md-3 col-sm-6">
-                  <div class="footer__widget">
-                      <h6>Shopping</h6>
-                      <ul>
-                          <li><a href="#">Contact Us</a></li>
-                          <li><a href="#">Payment Methods</a></li>
-                          <li><a href="#">Delivary</a></li>
-                          <li><a href="#">Return & Exchanges</a></li>
-                      </ul>
-                  </div>
-              </div>
-              <div class="col-lg-3 offset-lg-1 col-md-6 col-sm-6">
+
+              <div class="col-lg-6  col-md-6 col-sm-6">
                   <div class="footer__widget">
                       <h6>NewLetter</h6>
                       <div class="footer__newslatter">
@@ -50,11 +29,8 @@
               <div class="col-lg-12 text-center">
                   <div class="footer__copyright__text">
                       <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                      <p>Copyright ©
-                          <script>
-                              document.write(new Date().getFullYear());
-                          </script>2020
-                          All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                      <p>Copyright © 2017
+                          All rights reserved <i class="fa fa-heart-o" aria-hidden="true"></i> by <?= '' ?></a>
                       </p>
                       <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                   </div>
@@ -68,9 +44,11 @@
   <div class="search-model">
       <div class="h-100 d-flex align-items-center justify-content-center">
           <div class="search-close-switch">+</div>
-          <form class="search-model-form">
-              <input type="text" id="search-input" placeholder="Search here.....">
-          </form>
+          <?php $form = \yii\widgets\ActiveForm::begin(['method' => 'get', 'action' => 'index.php?r=site%2Fshop', 'class' => 'search-model-for']); ?>
+          <input type="text" id="search-input" placeholder="Search here.....">
+          <?php \yii\widgets\ActiveForm::end(); ?>
+
+
       </div>
   </div>
   <!-- Search End -->
--- views/site/shop.php
+++ views/site/shop.php
@@ -1,6 +1,7 @@
     <?php
 
-    use yii\helpers\Html; ?>
+    use yii\helpers\Html;
+    ?>
     <!-- Breadcrumb Section Begin -->
     <section class="breadcrumb-option">
         <div class="container">
@@ -74,41 +75,7 @@
                     <div class="row">
                         <?php foreach ($models as $model) : ?>
                             <div class="col-lg-4 col-md-6 col-sm-6">
-                                <div class="product__item">
-                                    <div class="product__item__pic set-bg" data-setbg="<?= Yii::getAlias('@web') . "/" . $model->thumbnail ?>">
-                                        <ul class="product__hover">
-                                            <!-- <li><a href="#"><img src="img/icon/heart.png" alt=""></a></li>
-                                            <li><a href="#"><img src="img/icon/compare.png" alt=""> <span>Compare</span></a>
-                                            </li>
-                                            <li><a href="#"><img src="img/icon/search.png" alt=""></a></li> -->
-                                        </ul>
-                                    </div>
-                                    <div class="product__item__text">
-                                        <h6><?= $model->name ?></h6>
-
-                                        <?= Html::a(Yii::t('app', 'Update'), ['/product/view', 'id' => $model->id], ['class' => 'add-cart']) ?>
-
-                                        <div class="rating">
-                                            <i class="fa fa-star-o"></i>
-                                            <i class="fa fa-star-o"></i>
-                                            <i class="fa fa-star-o"></i>
-                                            <i class="fa fa-star-o"></i>
-                                            <i class="fa fa-star-o"></i>
-                                        </div>
-                                        <h5><?= $model->purchasing_price ?> JOD</h5>
-                                        <div class="product__color__select">
-                                            <!-- <label for="pc-4">
-                                                <input type="radio" id="pc-4">
-                                            </label>
-                                            <label class="active black" for="pc-5">
-                                                <input type="radio" id="pc-5">
-                                            </label>
-                                            <label class="grey" for="pc-6">
-                                                <input type="radio" id="pc-6">
-                                            </label> -->
-                                        </div>
-                                    </div>
-                                </div>
+                                <?= $this->render('/components/product', ['model' => $model]); ?>
                             </div>
                         <?php endforeach; ?>
 
